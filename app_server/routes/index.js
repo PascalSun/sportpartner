77228e70b771f164d8d84613396f5eb24b08ae4b
PascalSun <sunqiang93@163.com>
2017-05-19 14:03:05 +0800

    add basic login and register function

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,17 +1,44 @@
+require('../models/db');
 var express = require('express');
 var router = express.Router();
 var ctrlmain = require('../controller/main');
-var adduser = require('../controller/register');
+var passport = require('passport');
+var Account = require('../models/account');
+// var adduser = require('../controller/register');
 /* GET home page. */
-router.get('/',ctrlmain.index);
-router.get('/login',function(req,res){
-    res.render('login',{});
+router.get('/', function(req, res) {
+  res.render('index', {user: req.user});
 });
-router.get('/register',function(req,res){
-    res.render('register',{});
+
+router.get('/register', function(req, res) {
+  res.render('register', {});
 });
-router.post('/register',adduser.newUser);
 
+router.post('/register', function(req, res, next) {
+  console.log('registering user');
+  Account.register(new Account({username: req.body.username}), req.body.password, function(err) {
+    if (err) {
+      console.log('error while user register!', err);
+      return next(err);
+    }
 
+    console.log('user registered!');
+
+    res.redirect('/');
+  });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', {user: req.user});
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
